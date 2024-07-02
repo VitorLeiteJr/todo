8f03baa7e0b59e1a0931c75a132ecd302dae521b
VitorLeiteJr <96vitorleite@gmail.com>
2024-07-02 14:02:37 -0300

    feat: Delete tasks

---
+++ components/shared/DeleteTodo.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+ import { deleteTodo } from "@/app/actions/todoActions";
+import { todoType } from "@/types/todoType";
+import Button from "../ui/button";
+import Form from "../ui/form";
+import Input from "../ui/input";
+import { BsFillTrashFill } from "react-icons/bs";
+
+const DeleTodo = ({todo}: todoType) => {
+    return (
+        <Form action={deleteTodo}>
+            <Input type="hidden"
+            name="inputId"
+            value={todo.id}/>
+            <Button
+            actionButton
+            text={<BsFillTrashFill/>}
+            type="submit"/>
+        </Form> 
+    );
+}
+ 
+export default DeleTodo;
